ac62b7bebf2c3cfb9a9912714c34997aef06e6ab
archiron <arnaud.chiron@cern.ch>
2021-05-11 15:02:32 +0200

    version 2021.05.11-03
    minor correction on extractKS.sh

--- extractKS.sh
+++ extractKS.sh
@@ -2,7 +2,7 @@
 # This file is called . extractKS.sh & NOT ./extractKS.sh
 #
 
-git clone https://github.com/archiron/KS-Tools KS_Tools
+git clone https://github.com/archiron/KS_Tools KS_Tools
 git clone https://github.com/archiron/ChiLib_CMS_Validation ChiLib_CMS_Validation
 
 cd KS_Tools/

38bcaf32c34d2e435dcfc9458547594627b9b17e
archiron <arnaud.chiron@cern.ch>
2021-05-11 15:07:55 +0200

    version 2021.05.11-05
    reorganization

--- installtKSTools.sh
+++ installtKSTools.sh
@@ -3,7 +3,7 @@
 #
 
 git clone https://github.com/archiron/KS_Tools KS_Tools
+cd KS_Tools/
 git clone https://github.com/archiron/ChiLib_CMS_Validation ChiLib_CMS_Validation
 
-cd KS_Tools/
-
+cd GenExtract
